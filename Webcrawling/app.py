67573cbae8102b8e3e22eda53c0b7af3fa3253c8
AYoungSn <dudtjs0920@naver.com>
2020-04-18 21:09:19 +0900

    weather data edit

--- Webcrawling/app.py
+++ Webcrawling/app.py
@@ -5,11 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    req = Request('https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8')
+    url ='https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8'
+
+    req = Request(url)
     page = urlopen(req)
     html = page.read()
     soup = bs4.BeautifulSoup(html, 'html5lib')
-    return render_template("index.html",cel=soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text)
+    t = soup.find('li', class_='date_info today').find('dd').text.split('/')
+    temperature = []
+    for i in t:
+        temperature.append(int(i.split('°')[0]))
+    temperature.append(soup.find('li', class_='date_info today').find('dd').text)
+
+    return render_template("index.html", list=temperature)
 
 if __name__ == '__main__':
     app.run()
