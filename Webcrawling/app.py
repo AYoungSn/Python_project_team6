1a02209e6f1ad6967785dc51d68e02943817bc44
AYoungSn <dudtjs0920@naver.com>
2020-04-24 19:58:59 +0900

    외부접속/localhost

--- Webcrawling/app.py
+++ Webcrawling/app.py
@@ -41,4 +41,5 @@ def hello():
     return render_template("index.html", list=list)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    #app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
