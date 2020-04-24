a9b27331e4039a768b1abbdb1f28c28b5937bb53
AYoungSn <dudtjs0920@naver.com>
2020-04-24 13:57:50 +0900

    변수 타입&데이터 종류 수정

--- Webcrawling/app.py
+++ Webcrawling/app.py
@@ -5,19 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    # Webpage html 읽어오는 코드
     url ='https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8'
 
     req = Request(url)
     page = urlopen(req)
     html = page.read()
     soup = bs4.BeautifulSoup(html, 'html5lib')
-    t = soup.find('li', class_='date_info today').find('dd').text.split('/')
     temperature = []
-    for i in t:
-        temperature.append(int(i.split('°')[0]))
-    temperature.append(soup.find('li', class_='date_info today').find('dd').text)
 
-    return render_template("index.html", list=temperature)
+    # 웹 크롤링 부분
+    info_temp=soup.find('ul',class_='info_list').text.split('   ')
+    print(info_temp)
+    info_temp[1]=info_temp[1].strip().split(' ')
+    t=info_temp[1][0].split('/')
+    for i in t:
+        temperature.append(int(i[:-1]))
+    list={'cast_txt':info_temp[0].strip().split(',')[0], 'temperature': info_temp[1][0],
+          'min_t':temperature[0],'max_t': temperature[1], 'sensible':info_temp[1][2]}
+    print(list)
+    return render_template("index.html", list=list)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
